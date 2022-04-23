a25b14aedb85c94ca5b60239b1e8634bee75a5ee
Alexey Malyukin <alexeymalyukin@gmail.com>
2022-04-24 01:51:55 +0300

    Greeting added

---
+++ Clock.js
@@ -0,0 +1,46 @@
+
+function showtime() {
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    
+    if (h < 10){
+        h = "0" + h;
+    } 
+    if (m < 10){
+        m = "0" + m;
+    } 
+    if (s < 10){
+        s = "0" + s;
+    } 
+        
+    var time = h + ":" + m + ":" + s;
+
+    document.getElementById("MyClock").innerText = time;
+    document.getElementById("MyClock").textContent = time;
+        
+    setTimeout(showtime, 1000);
+    
+}
+
+showtime();
+
+function greetMe() {
+    var greeting = "";
+    var date = new Date();
+    var h = date.getHours();
+    if (h > 4 | h < 12) {
+        greeting = "Good Morning!";
+    } else if (h < 11 | h > 18) {
+        greeting = "Good afternoon!";
+    } else if (h > 17 | h >= 23) {
+        greeting = "Good evening";
+    } else (h >= 0 | h < 5); {
+        greeting = "Good night!";
+    }
+    document.getElementById("Greeting").textContent = greeting + " We are from Ukraine!";
+
+    setTimeout(greetMe, 1000);
+}
+greetMe();
