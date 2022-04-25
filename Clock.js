ca50d2ec17c598df9b1c29e2e3d8155551d7da49
Alexey Malyukin <alexeymalyukin@gmail.com>
2022-04-25 11:24:03 +0300

    Search bar added

--- Clock.js
+++ Clock.js
@@ -30,12 +30,12 @@ function greetMe() {
     var date = new Date();
     var h = date.getHours();
     var greeting = null;
-        if (h > 4 || h < 12) {
+        if (h > 4 & h < 12) {
             greeting = "Good Morning!";
-        } else if (h > 11 || h < 18) {
+        } else if (h > 11 & h < 18) {
             greeting = "Good afternoon!";
         } else if (h > 17) {
-            greeting = "Good evening";
+            greeting = "Good evening!";
         } else if (h < 5) {
             greeting = "Good night!";
         }
@@ -44,3 +44,38 @@ function greetMe() {
     setTimeout(greetMe, 1000);
 }
 greetMe();
+
+
+var themes = [ 'theme1.jpg', 'theme2.jpg', 'theme3.jpg'];
+index=0;
+themes[0]= "<img src='theme1.jpg'>";
+themes[1]= "<img src='theme2.jpg'>";
+themes[2]= "<img src='theme3.jpg'>";
+index = Math.floor(Math.random * themes.length);
+
+$().ready(function(){
+    var i = 0;
+    
+  //   Background Images
+    var images = [
+      'theme1.jpg',
+      'theme2.jpg',
+      'theme3.jpg'
+    ]
+    
+  //   pick body element ID
+    var image = $('#imageChange')
+    
+  //   set initial background-image
+    image.css('background-image', 'url(theme3.jpg)')
+    
+  //   change image every after 5 seconds
+    setInterval(function(){
+      image.fadeOut(1000, function(){
+        image.css('background-image', 'url(' + images [i++] +')')
+        image.fadeIn(1000)
+      })
+      if(1 == images.length)
+        i = 0
+    }, 5000)
+  })
